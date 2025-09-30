223ada079c43e87a7e6a0ca322d3704f9db733c4
Alexandre-Mir <110476249+Alexandre-Mir@users.noreply.github.com>
2025-09-29 21:57:22 -0300

    Adiciona Gruntfile com tarefas de LESS e Uglify

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+grunt.initConfig({
+	less: {
+		development: {
+			files: {
+				"dist/styles/main.css": "src/styles/main.less",
+			},
+		},
+	},
+	uglify: {
+		build: {
+			src: "src/scripts/app.js",
+			dest: "dist/scripts/app.min.js",
+		},
+	},
+});
+
+grunt.loadNpmTasks("grunt-contrib-uglify");
+grunt.loadNpmTasks("grunt-contrib-less");
+
+grunt.registerTask("default", ["uglify", "less"]);
